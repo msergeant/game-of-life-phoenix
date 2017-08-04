f14da5a2bcf46a782e97debfa46156b7043a5409
Mike Sergeant <sergeant.mj@gmail.com>
2017-08-03 22:43:37 -0400

    Change controller to start calling world

--- web/controllers/world_iteration_controller.ex
+++ web/controllers/world_iteration_controller.ex
@@ -1,7 +1,12 @@
 defmodule GameOfLife.WorldIterationController do
   use GameOfLife.Web, :controller
 
-  def next(conn, _params) do
-    render conn, live_cells: []
+  def next(conn, params) do
+    next_world = params
+    |> Map.get("live_cells")
+    |> World.new
+    |> World.to_list
+
+    render conn, live_cells: next_world
   end
 end
